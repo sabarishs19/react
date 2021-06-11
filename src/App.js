3dc8925865bbc0baa9dfa1376c012388b8038332
Sabarish19 <84761863+tb-co@users.noreply.github.com>
2021-06-11 17:31:38 +0000

    d

--- src/App.js
+++ src/App.js
@@ -1,70 +1,26 @@
-import { Container, Flex, Spinner, VStack } from "@chakra-ui/core";
-import React, { useEffect, useState } from "react";
-import Navbar from "./components/navbar";
-import Post from "./components/post";
-import db from "./lib/firebase";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-  const [posts, setPosts] = useState([]);
-  const [isLoading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Hook to handle the initial fetching of posts
-
-    db.collection("posts")
-      .orderBy("createdAt", "desc")
-      .get()
-      .then((querySnapshot) => {
-        const data = querySnapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-
-        setPosts(data);
-        setLoading(false);
-      });
-  }, []);
-
-  useEffect(() => {
-    // Hook to handle the real-time updating of posts whenever there is a
-    // change in the datastore (https://firebase.google.com/docs/firestore/query-data/listen#view_changes_between_snapshots)
-
-    db.collection("posts")
-      .orderBy("createdAt", "desc")
-      .onSnapshot((querySnapshot) => {
-        const _posts = [];
-
-        querySnapshot.forEach((doc) => {
-          _posts.push({
-            id: doc.id,
-            ...doc.data(),
-          });
-        });
-
-        setPosts(_posts);
-      });
-  }, []);
-
-  if (isLoading) {
-    return (
-      <Flex minH="100vh" justifyContent="center" alignItems="center">
-        <Spinner />
-      </Flex>
-    );
-  }
-
+function App() {
   return (
-    <>
-      <Navbar />
-      <Container maxW="md" centerContent p={8}>
-        <VStack spacing={8} w="100%">
-          {posts.map((post) => (
-            <Post post={post} key={post.id} />
-          ))}
-        </VStack>
-      </Container>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code>!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
